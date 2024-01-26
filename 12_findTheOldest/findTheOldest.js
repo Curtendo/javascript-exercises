b4014923492352d9c75bdb082d908a47aab99503
Curtis CHROME <mrmr450@gmail.com>
2024-01-26 11:54:44 +0900

    Complete 12 Find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    console.table(people.map(person => ({ ...person })));
+
+    people.forEach(person => {
+        if (!person.yearOfDeath) {
+            const currentYear = new Date().getFullYear();
+            person.yearOfDeath = currentYear;
+        }
+    });
+    console.table(people.map(person => ({ ...person })));
+
+    people.sort((a, b) => {
+        let aAge = a.yearOfDeath - a.yearOfBirth;
+        let bAge = b.yearOfDeath - b.yearOfBirth;
+        return aAge > bAge ? -1 : 1;
+    });
+    console.table(people.map(person => ({ ...person })));
+    return people[0];
 };
 
 // Do not edit below this line
